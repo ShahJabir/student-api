6cf2bb4940166ab0a736fda004a8d2a96f8fd3f0
ShahJabir <shahjabirofficial@gmail.com>
2025-12-28 21:31:24 +0600

    feat: setup config, router and server in main.go fix: fixed address config handling

--- cmd/student-api/main.go
+++ cmd/student-api/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/ShahJabir/student-api/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// load config
+	cfg := config.MustLoad()
+	// database setup
+	// setup router
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to student api"))
+	})
+	// setup server
+	server := http.Server{
+		Addr:    cfg.Address,
+		Handler: router,
+	}
+	fmt.Printf("Server Started %s \n", cfg.HTTPServer.Address)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalf("failed to start server: %s", err)
+	}
 }
