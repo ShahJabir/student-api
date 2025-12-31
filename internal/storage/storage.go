8b2abf0697538265d60d051d591b9fe46f32f603
ShahJabir <shahjabirofficial@gmail.com>
2025-12-31 19:54:41 +0600

    feat: GetById endpoint added

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,5 +1,8 @@
 package storage
 
+import "github.com/ShahJabir/student-api/internal/types"
+
 type Storage interface {
 	CreateStudent(name string, email string, age int) (int64, error)
+	GetStudentById(Id int64) (types.Student, error)
 }
