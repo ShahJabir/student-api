548cfc1816d1d011a4bd038e928b1239fb9e8940
ShahJabir <shahjabirofficial@gmail.com>
2026-01-01 00:14:35 +0600

    feat: get all students list api endpoint added

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -5,4 +5,5 @@ import "github.com/ShahJabir/student-api/internal/types"
 type Storage interface {
 	CreateStudent(name string, email string, age int) (int64, error)
 	GetStudentById(Id int64) (types.Student, error)
+	GetStudents() ([]types.Student, error)
 }
